a456deed065f65229c4a93e05816345bf0a15a66
Okorie Jude Ifeanyi <jude58005@gmail.com>
2022-10-12 14:47:31 +0100

    fixing sql comment stopping select privilege from executing

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,5 +9,5 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant user hbnb_dev all privileges
 GRANT ALL PRIVILEGES on `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
---Grant user hbnb_dev SELECT privileges on 'performance_schema'
-GRANT SELECT on `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- Grant user hbnb_dev SELECT privileges on 'performance_schema'
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
