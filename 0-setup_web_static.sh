ca0fec6859835778c214e61fe5b3d8a7f8774d26
Okorie Jude Ifeanyi <jude58005@gmail.com>
2022-10-30 23:21:35 +0100

    properly install nginx. add content to html file. add sudo.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 # Script to setup web server for deployment
 sudo apt-get update
-sudo apt-get nginx
-mkdir /data/
-mkdir /data/web_static/
-mkdir /data/web_static/releases/
-mkdir /data/web_static/shared/
-mkdir /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-ln -f /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i "25i \\\tserver {\tlocation /hbnb_static {\talias /data/web_static/current/;\t}\t}" /etc/nginx/nginx.conf
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    ALX wan kill me
+  </body>
+</html>" >> /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -i "25i \\\tserver {\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}\t}" /etc/nginx/nginx.conf
 sudo service nginx restart
