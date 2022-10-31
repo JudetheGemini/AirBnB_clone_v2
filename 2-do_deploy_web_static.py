d5a54ab398df923c8e7b5a57a130a9aaf968662d
Okorie Jude Ifeanyi <jude58005@gmail.com>
2022-10-31 11:10:02 +0100

    adding function from first python file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,21 @@ env.user = 'ubuntu'
 env.hosts = ['34.204.177.254', '18.207.112.39']
 
 
+def do_pack():
+    """
+        Function that generates a .tgz archive of all static files
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('sudo mkdir -p ./versions')
+    path = './versions/web_static_{}.tgz'.format(now)
+    local('sudo tar -czvf {}.tgz web_static'.format(path))
+    name = '{}.tgz'.format(path)
+    if name:
+        return name
+    else:
+        return None
+
+
 def do_deploy(archive_path):
     """
         script that distributes an archive file to web server
@@ -31,3 +46,5 @@ def do_deploy(archive_path):
         run('sudo rm -rf /data/web_static/current')
         run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.
             format(folder))
+        print('New version deployed!')
+        return True
