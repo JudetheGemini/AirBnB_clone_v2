4ecb84fe2543b99ab98e2719a584792400e9dd15
Okorie Jude Ifeanyi <jude58005@gmail.com>
2022-11-11 12:38:30 +0100

    specify variable type?

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,13 +23,13 @@ def hbnb():
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def variable(text):
+@app.route("/c/<string:text>", strict_slashes=False)
+def c(text):
     """
-        Displays 'C' followed by teh value of the text variable
+        Displays 'C' followed by the value of the text variable
     """
     return f"C {text}".replace('_', ' ')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
